fd1aee4f6130dfc63db8ef10548861741257d0e9
Davi de Sousa <deivede@gmail.com>
2020-12-21 17:18:53 -0300

    Add profile picture update and index to friends JSON

--- src/twitter/update_friend.py
+++ src/twitter/update_friend.py
@@ -1,10 +1,20 @@
 import tweepy
 from keys import *
 from config import *
+import requests
 
-def updateProfileImage(user, friendId):
-    id = friendId
-    friendData = api.get_user(user_id=id)
+def updateProfileImage(user):
+    rawFriends = db.users.find({"screen_name": user}, {"twitter": 1})
+    friends = rawFriends[0]["twitter"]
 
-    friendPic = friendData.__dict__['_json']['profile_image_url']
-    db.users.find_one_and_update({'screen_name': user}, {'$set': {'twitter.' + id + '.profile_image_url': friendPic}})
+    for friend in friends:
+        friendPic = friends[friend]['profile_image_url']
+        picRequest = requests.get(friendPic)
+
+        if picRequest.status_code == 404:
+            friendData = api.get_user(user_id=friend)
+
+            newPic = friendData.__dict__['_json']['profile_image_url']
+            db.users.find_one_and_update({'screen_name': user}, {'$set': {'twitter.' + friend + '.profile_image_url': newPic}})
+
+            print(friend + "pic updated")
